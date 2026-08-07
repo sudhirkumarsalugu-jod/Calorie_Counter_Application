f3282d2be31454621f2cd67c58f0cd8f6284c2fe
Sudhir Kumar <sudhirkumar.salugu@gmail.com>
2026-08-07 12:20:48 +0530

    Added page title

--- Calorie_counter.py
+++ Calorie_counter.py
@@ -8,7 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 st.set_page_config(
-    layout= "centered")
+    layout= "centered"
+    page_title= "Calorie Counter",
+    page_icon= "🏥")
 
 st.markdown("""
 <h1 style="
